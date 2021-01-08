52056969aa945c0560945d854dfaa63a3efc14a5
uwase-diane <diane.uwase13@gmail.com>
2021-01-08 02:50:59 -0500

    changed class comment name

--- insta/admin.py
+++ insta/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Image, Profile, Comment, Follow
+from .models import Image, Profile, Comments, Follow
 
 admin.site.register(Image)
 admin.site.register(Profile)
 admin.site.register(Follow)
-admin.site.register(Comment)
+admin.site.register(Comments)
