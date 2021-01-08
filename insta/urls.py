810dc3c2cefc36bd16ca1979ec6a2800defd7c06
uwase-diane <diane.uwase13@gmail.com>
2021-01-08 02:59:49 -0500

    added urls pages

--- insta/urls.py
+++ insta/urls.py
@@ -1,12 +1,20 @@
-from django.urls import path
-from . import views
+from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
-urlpatterns = [
-    path('/', views.index, name = 'index'),
-    path('search/',views.search, name = 'search')
+
+urlpatterns=[
+    url('^$',views.welcome,name = 'welcome'),
+    url(r'^new/upload$', views.upload_image, name='upload'),
+    url(r'^new/profile$', views.add_profile, name='edit'),
+    url(r'^myprofile$', views.my_profile, name='myprofile'),
+    url(r'^comment/(\d+)/$', views.add_comment, name='comment'),
+    url(r'^search/',views.search_users, name = 'searchs'),
+    url(r'^like/(\d+)/$',views.likes, name = 'like'),
+    url(r'^follow/(\d+)/$',views.following, name = 'follow'),
+    url(r'^othersprofile/(\d+)/$', views.others_profile, name='othersprofile'),
 ]
-
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    
