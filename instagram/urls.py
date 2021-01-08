a40731b380f941dc1d4e78cdc58aed40ae14a100
uwase-diane <diane.uwase13@gmail.com>
2021-01-08 03:05:37 -0500

    added admin ulr

--- instagram/urls.py
+++ instagram/urls.py
@@ -13,14 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url,include
 from django.contrib import admin
-from django.urls import path,include
 from django.contrib.auth import views 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('insta.urls')),
-    path('accounts/',include('registration.backends.simple.urls')),
-    path('logout/', views.logout, {"next_page":'/'}),
+    url(r'^admin/', admin.site.urls),
+    url(r'^',include('insta.urls')),
+    url(r'^tinymce/', include('tinymce.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}), 
 ]
-
