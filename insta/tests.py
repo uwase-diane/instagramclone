5df0ddb0e6b356bfc3b360fbea858143bf97efca
uwase-diane <diane.uwase13@gmail.com>
2021-01-08 02:53:22 -0500

    added unit tests

--- insta/tests.py
+++ insta/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Profile
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class TestProfile(TestCase):
+    def setUp(self):
+
+        self.profile_test = Profile(id = 1, name = 'image', profilepic = 'pic1.png', bio = 'happy new year', user = self.user)
+
+        def test_instance(self):s
+            self.assertTrue(isinstance(self.profile_test, Profile))
+
+        def test_save_profile(self):
+            self.profile_test.save_profile()
+            profiles = Profile.objects.all()
+            self.assertTrue(len(after) > 0)
+
