9f635292e69e4755f16f43576cd9c692da81baf8
uwase-diane <diane.uwase13@gmail.com>
2021-01-09 10:04:05 -0500

    heroku deployment

--- instagram/settings.py
+++ instagram/settings.py
@@ -93,8 +93,7 @@ if config('MODE')=="dev":
            'NAME': config('DB_NAME'),
            'USER': config('DB_USER'),
            'PASSWORD': config('DB_PASSWORD'),
-           'HOST': config('DB_HOST'),
-           'PORT': '',
+           
        }
        
    }
