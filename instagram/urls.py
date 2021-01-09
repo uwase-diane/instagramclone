bc09339ecee87a1887177a4ba80380e617860b20
uwase-diane <diane.uwase13@gmail.com>
2021-01-09 03:02:15 -0500

    remove a symbol on insta app url

--- instagram/urls.py
+++ instagram/urls.py
@@ -19,7 +19,7 @@ from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^',include('insta.urls')),
+    url(r'',include('insta.urls')),
     url(r'^tinymce/', include('tinymce.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}), 
